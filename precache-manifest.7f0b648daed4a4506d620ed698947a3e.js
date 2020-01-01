9a3b5e06274f984a1dcb61bca0698ab63e012336
Muhammad Ali <ali.manzoor741@gmail.com>
2020-01-01 16:38:28 +0400

    Updates

---
+++ precache-manifest.7f0b648daed4a4506d620ed698947a3e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c236e74d3dc902e5602042f8f1d4c57",
+    "url": "/robo-search/index.html"
+  },
+  {
+    "revision": "ed00ff190f2ca09980d7",
+    "url": "/robo-search/static/css/main.607d6359.chunk.css"
+  },
+  {
+    "revision": "9d7eb7ef8c011505ceca",
+    "url": "/robo-search/static/js/2.745ad1a4.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/robo-search/static/js/2.745ad1a4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ed00ff190f2ca09980d7",
+    "url": "/robo-search/static/js/main.d46d1f11.chunk.js"
+  },
+  {
+    "revision": "7e4069a818d1e37a0446",
+    "url": "/robo-search/static/js/runtime-main.a92f0506.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robo-search/static/media/SEGA.12a3db8b.woff"
+  }
+]);
